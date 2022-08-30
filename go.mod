30cf5ffac4812ac865d69bd65710245b5be653a4
blessedmadukoma <blessedmadukoma@gmail.com>
2022-08-30 16:32:17 +0100

    added home handler and server

--- go.mod
+++ go.mod
@@ -4,3 +4,5 @@ module go-deploy
 go 1.18
 
 require github.com/go-chi/chi v1.5.4
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
 )
 
 // home handler is unexported due to no other package requiring the handler
@@ -17,22 +18,30 @@ func home(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
-// portNumber
-var portNumber = os.Getenv("PORT")
-
 func main() {
+
+	// load our env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Unable to load env file")
+	}
+
 	// create a new router
 	router := chi.NewRouter()
 	router.HandleFunc("/", home)
 
+	// portNumber
+	var portNumber = os.Getenv("PORT")
+
+	portNumber = ":" + portNumber
 	// create our server
 	srv := &http.Server{
-		Addr: portNumber,
+		Addr:    portNumber,
 		Handler: router,
 	}
 
 	fmt.Printf("Starting server on port %s\n", portNumber)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
